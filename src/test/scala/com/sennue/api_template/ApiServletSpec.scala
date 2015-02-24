c431c107030a828b3eaf07c2f95dd68468bf1abe
Brendan A R Sechter <sgeos@hotmail.com>
2015-02-24 11:23:52 -0600

    Added unit testing methods to verify JSON contents.

--- src/test/scala/com/sennue/api_template/ApiServletSpec.scala
+++ src/test/scala/com/sennue/api_template/ApiServletSpec.scala
@@ -1,16 +1,21 @@
 package com.sennue.api_template
 
 import org.scalatra.test.specs2._
+import org.json4s.{DefaultFormats, Formats}
+import org.json4s.jackson.JsonMethods._
+import scala.reflect._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html
 class ApiServletSpec extends ScalatraSpec {
 
+  protected implicit val jsonFormats: Formats = DefaultFormats
+
   addServlet(classOf[ApiServlet], "/*")
 
   val ECHO_REQUEST_GOOD = """{"id":"unit-test","message":"Message."}"""
-  val ECHO_RESPONSE_GOOD = """{"id":"unit-test","message":"Echo this message!","success":true}"""
+  val ECHO_RESPONSE_GOOD = """{"success":true,"id":"unit-test","message":"Message."}"""
   val ECHO_REQUEST_BAD = """{}"""
-  val ECHO_RESPONSE_BAD = """{"error":"MappingException","message":"No usable value for id\nDid not find value which can be converted into java.lang.String","success": false}"""
+  val ECHO_RESPONSE_BAD = """{"success":false,"error":"MappingException","message":"No usable value for id\nDid not find value which can be converted into java.lang.String"}"""
 
   def is =
     "GET / on ApiServlet"                        ^
@@ -18,13 +23,19 @@ class ApiServletSpec extends ScalatraSpec {
     "GET /error on ApiServlet"                   ^
     "should return status 500"                   ! getStatusCode("/error/",500)^
     "POST /echo on ApiServlet"                   ^
-    "should return status 200"                   ! postStatusCode("/",ECHO_REQUEST_GOOD,200)^
+    "should return status 200"                   ! postStatusCode("/echo",ECHO_REQUEST_GOOD,200)^
     "POST /echo on ApiServlet"                   ^
-    "should return message json"                 ! postResponseBody("/",ECHO_REQUEST_GOOD,ECHO_RESPONSE_GOOD)^
+    "should return message json"                 ! postResponseBody("/echo",ECHO_REQUEST_GOOD,ECHO_RESPONSE_GOOD)^
     "POST /echo on ApiServlet"                   ^
-    "bad JSON should return status 500"          ! postStatusCode("/",ECHO_REQUEST_BAD,500)^
+    "bad JSON should return status 500"          ! postStatusCode("/echo",ECHO_REQUEST_BAD,500)^
     "POST /echo on ApiServlet"                   ^
-    "bad JSON should return error message json"  ! postResponseBody("/",ECHO_REQUEST_BAD,ECHO_RESPONSE_BAD)^
+    "bad JSON should return error message json"  ! postResponseBody("/echo",ECHO_REQUEST_BAD,ECHO_RESPONSE_BAD)^
+    "POST /echo on ApiServlet"                   ^
+    "bad JSON should return error message json"  ! postResponseBodyKeyValue("/echo",ECHO_REQUEST_BAD,"success",false)^
+    "POST /echo on ApiServlet"                   ^
+    "bad JSON should return error message json"  ! postResponseBodyKeyValue("/echo",ECHO_REQUEST_BAD,"error","MappingException")^
+    "POST /echo on ApiServlet"                   ^
+    "bad JSON should return error message json"  ! postResponseBodyKeyOfType[String]("/echo",ECHO_REQUEST_BAD,"message")^
                                               end
 
   def getStatusCode(uri:String, code:Int) = 
@@ -42,11 +53,25 @@ class ApiServletSpec extends ScalatraSpec {
       response.body must_== body
     }
 
-  // Reference: https://groups.google.com/forum/#!topic/scalatra-user/I_vPKT-twRY
-  def postJson[A](uri:String, body:String)(f: => A): A =
-    post(uri, body.getBytes("utf-8"), Map("Content-Type" -> "application/json"))(f)
+  def postResponseBodyKeyOfType[A](uri:String, json:String, key:String)(implicit m: Manifest[A]) = 
+    postJson(uri, json) {
+      (parse(response.body) \ key).extract[A].getClass() must_== getRuntimeClass[A]
+    }
+
+  def postResponseBodyKeyValue[A](uri:String, json:String, key:String, value:A)(implicit m: Manifest[A]) = 
+    postJson(uri, json) {
+      // extract value for key and make sure it is the expected value
+      (parse(response.body) \ key).extract[A] must_== value
+    }
 
   // Reference: https://groups.google.com/forum/#!topic/scalatra-user/I_vPKT-twRY
-  def postJson[A](uri:String, body:String, headers:Map[String, String])(f: => A): A =
-    post(uri, body.getBytes("utf-8"), Map("Content-Type" -> "application/json") ++ headers)(f)
+  def postJson[A](uri:String, json:String)(f: => A): A =
+    post(uri, json.getBytes("utf-8"), Map("Content-Type" -> "application/json"))(f)
+
+  // Reference: https://groups.google.com/forum/#!topic/scalatra-user/I_vPKT-twRY
+  def postJson[A](uri:String, json:String, headers:Map[String, String])(f: => A): A =
+    post(uri, json.getBytes("utf-8"), Map("Content-Type" -> "application/json") ++ headers)(f)
+
+  // got help in #scala on irc.freenode.net for this one
+  def getRuntimeClass[T: ClassTag]() = implicitly[ClassTag[T]].runtimeClass
 }
